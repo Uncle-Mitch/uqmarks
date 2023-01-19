c979eb03e488ad6c715c800b48bb4caa9299680f
NotMitch <damon.fok@outlook.com>
2023-01-19 13:27:13 +1000

    Sem2 2022 update

--- get_assessment.py
+++ get_assessment.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import os
+
 
 def return_url(code):
     return f'https://my.uq.edu.au/programs-courses/course.html?course_code={code}'
 
 def get_page(code:str, semester:str , year:str):
     semester = f'Semester {semester}'
-    offering = return_url(code) 
+    offering = return_url(code)
     headers = requests.utils.default_headers()
     headers.update(
         {
@@ -38,16 +40,14 @@ def get_table(section_code):
     page = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(page.text, 'html.parser')
-    
+
     rows = []
     box = soup.find_all('td', class_='text-center')
     row_holder = []
-    count = 0
     for child in box:
-        em = child.find('em')
+        em = child.find('em') # don't want embed text
         unwanted = ''
 
-        
         if em:
             unwanted = em.text
         edited_text = child.text.strip()
@@ -55,25 +55,47 @@ def get_table(section_code):
         edited_text = edited_text.replace('  ','')
         edited_text = edited_text.replace('\n','')
         if '%' in edited_text:
-            number, per, _ = edited_text.partition('%')
-            edited_text = number+per
+            number = edited_text.partition('%')[0]
+            edited_text = number + '%'
         row_holder.append(edited_text)
         if len(row_holder) % 4 == 0:
             count = len(row_holder)
             start = count - 4
             end = count-1
-            if len(row_holder) > 4:
-                if '%' not in row_holder[end-1]:
-                    continue
             rows.append(row_holder[start:end:2])
-   # df = pd.DataFrame(rows[1:], columns=rows[0])
-    return rows[1:]
-    
+    new_rows = []
+    for item in rows:
+        if not (item[0] == 'Assessment Task' and item[1] == 'Weighting'):
+            new_rows.append(item)
+    return new_rows
+
 
 def get_assessments(code:str, semester:str, year:str):
     course_profile = get_page(code, semester, year)
     section_code = course_profile.rpartition('/')[2]
     table = get_table(section_code)
+
+    # send to discord
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'User-Agent': 'My User Agent 1.0',
+        }
+    )
+
+    data = {
+    "content" : "",
+    "username" : "UQmarks"
+    }
+
+    data["embeds"] = [
+        {
+            "description" : f"{semester} {year}",
+            "title" : f"{code} - NEW CODE"
+        }
+    ]
+
+    result = requests.post(os.environ['LOG_LINK'], json = data, headers=headers)
     return table
 
 
