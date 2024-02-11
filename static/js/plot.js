9728d6d10748c415fe4a3eaa3b1e32887be96218
NotMitch <106473029+Uncle-Mitch@users.noreply.github.com>
2024-02-11 18:35:35 +1000

    Add progress bar

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,24 +1,10 @@
-function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-}
+obj = document.getElementById('analytics-iframe');
 
-document.addEventListener('DOMContentLoaded', function () {
-    var iframe = document.getElementById('analytics-iframe');
-
-    iframe.addEventListener('load', function () {
-        var iframeWindow = iframe.contentWindow;
-
-        // Check if the button is present inside the iframe
-        var semesterBtn = iframeWindow.document.getElementById('semester-btn');
-
-        if (semesterBtn) {
-            // Add a click event listener to the button inside the iframe
-            semesterBtn.addEventListener('click', function () {
-                console.log('Semester button clicked inside the iframe');
-                // Additional actions here
-            });
-        } else {
-            console.error('Button with ID "semester-btn" not found inside the iframe.');
-        }
-    });
+obj.contentWindow.addEventListener('DOMContentLoaded', function () {
+    //console.log(obj.contentWindow.document.body.scrollHeight);
+    //obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+    document.getElementById('spinner').style.display = 'none';
+    console.log('TESTING');
+    document.getElementById('analytics-iframe').style.visibility = 'visible';
 });
+
