8f6826edaa1979d1cc7ef67ca9c8f637828c06d7
Uncle Mitch <106473029+Uncle-Mitch@users.noreply.github.com>
2024-07-27 16:55:18 +1000

    Added logic for new course profiles

--- get_assessment.py
+++ get_assessment.py
@@ -53,7 +53,12 @@ def get_page(code:str, semester:str , year:str):
             return href
 
 
-def get_table(section_code):
+def get_table_old(section_code):
+    """Get table for courses before 2024 Semester 2
+
+    Args:
+        section_code (str): Course code
+    """
     headers = requests.utils.default_headers()
     headers.update(
         {
@@ -91,13 +96,71 @@ def get_table(section_code):
     df.loc[df['Assessment Task'].str.contains("||"), ['Assessment Task']] = df['Assessment Task'].str.partition('||')[0]
     return list(df.itertuples(index=False, name=None))
 
+def get_table(semester, year, section_code):
+    """Gets the assessment items for courses
+
+    Args:
+        semester (int): Semester for course
+        year (int): year for course
+        section_code (str): Course code
+    """
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'User-Agent': 'My User Agent 1.0',
+        }
+    )
+    
+    # For previous semesters, use old version of course profile
+    if (year == 2024 and semester == 1)or year < 2024:
+        return get_table_old(section_code)
+    
+    url = f'https://course-profiles.uq.edu.au/course-profiles/{section_code}#assessment'
+    page = requests.get(url, headers=headers)
+    html_content = page.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Remove <ul class="icon-list"> elements
+    for ul in soup.find_all('ul', class_='icon-list'):
+        ul.decompose()
+    
+    # Extract table headings + rows for pandas df
+    table = soup.find('table')
+    headers = [header.text.strip() for header in table.find_all('th')]
+    rows = []
+    for row in table.find('tbody').find_all('tr'):
+        cols = row.find_all('td')
+        rows.append([col.text.strip() for col in cols])
+        
+    df = pd.DataFrame(rows, columns=headers)
+    df = df.drop(columns=["Category", "Due date"])
+
+    # Edge case where weight = 0% and UQ left the weight 'blank'
+    df = df.dropna()
+    df['Weight'] = df['Weight'].astype(str)
+    
+    # Edge Case: Identify rows where Weighting does not contain "%" (e.g. DECO2200, DECO7200)
+    non_percentage_rows = df.loc[~df['Weight'].str.contains("%"), 'Weight']
+    
+    # If all entries are numeric and equal, convert them to percentages
+    if len(non_percentage_rows) > 0 and non_percentage_rows.apply(lambda x: x.isdigit()).all():
+        total_tasks = len(non_percentage_rows)
+        percentage = 100 / total_tasks
+        df.loc[~df['Weight'].str.contains("%"), 'Weight'] = f"{percentage:.2f}%"
+
+    # Convert weightings to the appropriate format.
+    df.loc[df['Weight'].str.contains("%"), ['Weight']] = df['Weight'].str.partition('%')[0]  + "%"
+    return list(df.itertuples(index=False, name=None))
+
 
 def get_assessments(code:str, semester:str, year:str):
     course_profile = get_page(code, semester, year)
     if course_profile is None:
         raise WrongSemesterError(code, semester)
     section_code = course_profile.rpartition('/')[2]
-    table = get_table(section_code)
+    year = int(year)
+    semester = int(semester)
+    table = get_table(semester, year, section_code)
 
     # send to discord
     headers = requests.utils.default_headers()
